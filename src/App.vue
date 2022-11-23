d8207201e2ea5884ff9b0095853cf0fe8f4f04b0
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-23 15:23:09 +0100

    Struttura di base

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<HeaderComp />
+	</div>
+</template>
+
+
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+	name: "App",
+	components: {
+		HeaderComp,
+	},
+};
+</script>
+
+<style lang='scss'>
+</style>
